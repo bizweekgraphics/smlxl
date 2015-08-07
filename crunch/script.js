9713500485a6ce2e6b1dea77bfb780bda935899f
Toph Tucker <tophtucker@gmail.com>
2015-08-07 15:00:29 -0400

    Second working draft of crunch
    using position:fixed instead, which is a tradeoff...

--- crunch/script.js
+++ crunch/script.js
@@ -8,16 +8,15 @@ grafs
 
 d3.select(window).on("scroll", function(d) {
   grafs
-    // .each(function(d) {
-    //   if (d.offsetTop > d3.select("body").node().scrollTop) {
-    //     return true;
-    //   } else {
-    //     d3.select(this)
-    //       .style("position", "fixed")
-    //       .style("top", 0);
-    //   }
-    // })
-    .style("top", function(d) { 
-      return Math.max(d.offsetTop, d3.select("body").node().scrollTop) + "px"; 
+    .each(function(d) {
+      if (d.offsetTop > d3.select("body").node().scrollTop) {
+        d3.select(this)
+          .style("position", "absolute")
+          .style("top", d.offsetTop+"px");
+      } else {
+        d3.select(this)
+          .style("position", "fixed")
+          .style("top", 0);
+      }
     })
 })
