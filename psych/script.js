6925e92f8a7fcde99a77cecc34ec0e773a9496c5
Toph Tucker <tophtucker@gmail.com>
2015-08-12 12:14:20 -0400

    trying things... flips inverting...

--- psych/script.js
+++ psych/script.js
@@ -15,15 +15,13 @@ d3.timer(function(t) {
 
   var scrollTop = body.node().scrollTop;
 
-  // body.style("-webkit-filter", "hue-rotate("+(Math.floor(body.node().scrollTop / 10) % 360)+"deg)");
-
-  for (var i = 0 ; i < 10; i++) {
-    ctx.fillStyle = i%2 == 0 ? "#000000" : "#ffffff";
-
+  var circles = d3.range(100).map(function(i) { return (10*(100-i) + t/10000) % 1000; }).sort(d3.descending);
+  circles.forEach(function(d, i) {
+    ctx.fillStyle = d3.rgb("hsl(0,0%,"+Math.floor(.5*(Math.sin(d*t/10000) + 1)*100)+"%)").toString();
     ctx.beginPath();
-    ctx.arc(innerWidth/2, innerHeight/2, (20*(10-i)) + 200*(Math.sin(i*t/5000)+1), 0, 2 * Math.PI, false);
+    ctx.arc(innerWidth/2, innerHeight/2, d, 0, 2 * Math.PI, false);
     ctx.fill();
-  }
+  })
 
   for (var i = hedArray.length - 1; i >= 0; i--) {
     ctx.globalAlpha = Math.min(1, Math.max(0, 1 - (scrollTop/200 - i)));
@@ -39,3 +37,14 @@ d3.timer(function(t) {
   ctx.globalAlpha = 1;
 
 });
+
+var invert;
+d3.select(window).on("scroll", function() {
+  var oldInvert = invert;
+  invert = Math.floor(body.node().scrollTop / 1000) % 2 == 1;
+  if(invert !== oldInvert) body.classed("invert", invert);
+})
+
+function cycleRadius(t,i) {
+  return (20*(10-i)) + 200*(Math.sin(i*t/5000)+1);
+}
